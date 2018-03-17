e7200763c659d60323d8906d7bd3e041257b4f9b
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-03-17 14:34:17 +0100

    chore(git): clean definition files

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type onCLickOut = (e: Event) => void;
-export declare type destroyClickOut = () => void;
-export default class ClickOut {
-    static bindCustomEvent(value: string | HTMLElement): destroyClickOut;
-    static bind(value: string | HTMLElement, onClickOut: onCLickOut): destroyClickOut;
-    static destroy(value: string | HTMLElement): void;
-    static element(el: string | HTMLElement): HTMLElement;
-}
